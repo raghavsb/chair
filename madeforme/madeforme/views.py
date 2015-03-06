f16c600ebd391724f80e897785b53a005b0305ac
Raghavendra <ragbadaskar@gmail.com>
2015-03-06 18:06:44 +0530

    06032015 commit

--- madeforme/madeforme/views.py
+++ madeforme/madeforme/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-# Landing Page Views - Common views not covered in other apps on the platform
+# Homepage Views - Common views not covered in other apps
 
 class HomepageView(TemplateView):
 	template_name='homepage.html'
