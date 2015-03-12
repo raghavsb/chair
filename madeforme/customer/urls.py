d8b80f0bade4282a58d70035a1cf8ebe552e603a
Raghavendra <ragbadaskar@gmail.com>
2015-03-12 08:19:36 +0530

    12032015am commit authview changes

--- madeforme/customer/urls.py
+++ madeforme/customer/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+
 from .views import BuyerSignupView, MakerSignupView, CustomerRedirectView, ProfileView
 
 # Because of strong linkages with django-authtools, template folder of this app is named registration
@@ -11,7 +12,7 @@ urlpatterns = patterns('',
     url(r'maker/signup/$', MakerSignupView.as_view(), name='makersignup'),
 
     # Authentication urls as per django-authtools
-    url(r'', include('authtools.urls')),
+    url(r'^', include('authtools.urls')),
 
     # Login redirect urls - distinguish between a buyer and a maker
     url(r'redirect/$', CustomerRedirectView.as_view(), name='customerredirect'),
